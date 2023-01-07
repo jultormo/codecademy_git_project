3bb1629dbbba0ea0f38d401d56237c2d07a14159
Julian Andres Torres Montufar <jultorre@bancolombia.com.co>
2023-01-06 19:15:52 -0500

    new files

---
+++ main.js
@@ -0,0 +1,6 @@
+
+fetch('https://api.chucknorris.io/jokes/random')
+// Exito
+    .then(response => response.json())  // convertir a json
+    .then(json => console.log(json))    //imprimir los datos en la consola
+    .catch(err => console.log('Solicitud fallida', err)); // Capturar errores
